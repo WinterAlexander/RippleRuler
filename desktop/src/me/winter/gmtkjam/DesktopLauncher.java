ba6e98a8ae6056ce3356577cc77de783f4297837
Alexander Winter <a.w1nter@hotmail.com>
2023-07-07 15:51:54 -0400

    Initial commit

---
+++ desktop/src/me/winter/gmtkjam/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package me.winter.gmtkjam;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import me.winter.gmtkjam.GMTKJam;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("GMTKJam");
+		new Lwjgl3Application(new GMTKJam(), config);
+	}
+}
